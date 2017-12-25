228bfe96999519d42c600190869da9ace92d0a46
asmaa.abbas@wework.com <asmaa.abbas@wework.com>
2017-12-25 16:28:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,13 @@ function happyHolidaysTo (name) {
 //   return `Happy holidays, ${name}!`;
 // };
 
-const happyHolidayTo = (holiday, name) => {
-  return `Happy ${holiday}, ${name}`;
-};
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+// const happyHolidayTo = (holiday, name) => {
+//   return `Happy ${holiday}, ${name}!`;
+// };
 
 const holidayCountdown = (holiday, numOfDays) => {
   return `It's ${days} days until ${holiday}`;
